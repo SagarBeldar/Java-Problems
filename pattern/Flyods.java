5f3cda32454f581dbeea4b2f3cc29987bdacdc4a
SagarBeldar <sagarbeldar03@gmail.com>
2023-11-29 19:50:21 +0530

    changes done

--- pattern/Flyods.java
+++ pattern/Flyods.java
@@ -5,7 +5,7 @@ public class Flyods {
         int n=5;
         int number=1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=i;j++){ 
              System.out.print(number+" ");
              number++;
             }
