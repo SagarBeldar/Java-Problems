232ed542463e7f90f212991e068111d7c5b06993
SagarBeldar <sagarbeldar03@gmail.com>
2024-03-10 01:05:48 +0530

    changes done

---
+++ Bit_Manipulation/basic.java
@@ -0,0 +1,27 @@
+package Bit_Manipulation;
+
+public class basic {
+    public static void main(String[] args) {
+        //-------------------Bitwise AND(&) Operator-------------------------
+        /*System.out.println(5&6);
+        System.out.println(7&8);
+        System.out.println(10&12);*/
+
+        //-------------------Bitwise OR(|) Operator-------------------------
+        // System.out.println(5|6);
+
+        //-------------------Bitwise XOR(^) Operator-------------------------
+        // System.out.println(5^6);
+
+        //-------------------Bitwise 1's Complement(~) Operator-------------------------
+        // System.out.println(~5);
+
+        //-------------------Bitwise left shift(<<) Operator-------------------------
+        // System.out.println(5<<2);
+        // System.out.println(6<<4);
+
+        //-------------------Bitwise Right shift(>>) Operator-------------------------
+        System.out.println(6>>4);
+        System.out.println(32>>4);
+    }
+}
